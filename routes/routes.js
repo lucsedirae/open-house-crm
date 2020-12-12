8e5c67ac375e278a7d091e3c2432d0bb4ad4a31e
Jon Deavers <jondeavers@gmail.com>
2020-12-12 14:52:12 -0500

    Pages and routes created

--- routes/routes.js
+++ routes/routes.js
@@ -6,4 +6,28 @@ module.exports = function(app) {
     app.get("/", (req, res) => {
         res.render("pages/index");
     });
+    app.get("/about", (req, res) => {
+        res.render("pages/about")
+    });
+    app.get("/devlog", (req, res) => {
+        res.render("pages/devlog")
+    });
+    app.get("/style", (req, res) => {
+        res.render("pages/style")
+    });
+    app.get("/features", (req, res) => {
+        res.render("pages/features")
+    });
+    app.get("/contributions", (req, res) => {
+        res.render("pages/contributions")
+    });
+    app.get("/getinvolved", (req, res) => {
+        res.render("pages/getinvolved")
+    });
+    app.get("/procontributions", (req, res) => {
+        res.render("pages/procontributions")
+    });
+    app.get("/login", (req, res) => {
+        res.render("pages/login")
+    });
 };
