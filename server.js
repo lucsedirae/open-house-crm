ffbbc349e07b18f7f963f011746b55916c16023e
Jon Deavers <jondeavers@gmail.com>
2021-01-21 11:58:25 -0500

    Heroku adjust

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ connectDB();
 //* Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.json({ msg: "Welcome to Open House CRM API" }));
-
 //*Define routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
