ed207389e2a4828f515da91f76d07fb42b69409f
Jon Deavers <jondeavers@gmail.com>
2021-01-21 11:39:01 -0500

    Prep for deploy

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/transactions", require("./routes/transactions"));
 
+//* Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //* Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 const PORT = process.env.PORT || 8080;
 
 //*Initialize server
