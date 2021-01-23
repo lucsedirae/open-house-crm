45eda460f7dde8a3774a1ce31a90b09d01863134
Tanner Kirkpatrick <twk4491@gmail.com>
2021-01-22 22:24:03 -0500

    api keys secure and map default center set to specific contact's lat and lng

--- client/src/components/modals/MapModal.js
+++ client/src/components/modals/MapModal.js
@@ -5,6 +5,8 @@ import MuiDialogTitle from "@material-ui/core/DialogTitle";
 import MuiDialogContent from "@material-ui/core/DialogContent";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
 import PublicTwoToneIcon from "@material-ui/icons/PublicTwoTone";
 import Typography from "@material-ui/core/Typography";
 import Map from "../map/Map";
@@ -60,9 +62,14 @@ export default function CustomizedDialogs({ contact }) {
   //* Returns JSX to DOM
   return (
     <div>
-      <IconButton variant="outlined" color="primary" onClick={handleClickOpen}>
-        <PublicTwoToneIcon style={{ color: "green" }} />
-      </IconButton>
+      <Button
+        startIcon={<PublicTwoToneIcon />}
+        style={{ color: "white", backgroundColor: "green" }}
+        onClick={handleClickOpen}
+      >
+        Map
+      </Button>
+
       <Dialog
         onClose={handleClose}
         aria-labelledby="customized-dialog-title"
--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const path = require("path");
-
 const app = express();
 
 //* Connect to database
