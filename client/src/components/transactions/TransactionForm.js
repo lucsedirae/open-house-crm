262cc9d22479611a8db5d82ee2da25bb638f6144
Jon Deavers <jondeavers@gmail.com>
2021-01-27 00:47:01 -0500

    Transaction form continued

--- client/src/components/transactions/TransactionForm.js
+++ client/src/components/transactions/TransactionForm.js
@@ -1,13 +1,22 @@
 //* Dependencies
 import React, { useEffect, useContext, useState } from "react";
+import DateFnsUtils from "@date-io/date-fns";
 
 //* Material UI components, hooks, and icons
 import Button from "@material-ui/core/Button";
 import Box from "@material-ui/core/Box";
+import FormControl from "@material-ui/core/FormControl";
+import Input from "@material-ui/core/Input";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
 
 //* State context
 import TransactionContext from "../../context/transactions/transactionContext";
@@ -112,29 +121,111 @@ const TransactionForm = ({ handleClose }) => {
         {current ? "Edit Transaction" : "Add Transaction"}
       </Typography>
 
-      <Box style={{ textAlign: "center" }}></Box>
-      <Button
-        variant="outlined"
-        type="submit"
-        color="primary"
-        fullWidth={true}
-        style={{ marginTop: "1rem", marginBottom: "1rem" }}
-        onClick={handleClose}
-      >
-        Submit
-      </Button>
-      {current && (
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Box style={{ textAlign: "center" }}>
+          <TextField
+            variant="standard"
+            required={true}
+            type="text"
+            id="standard-required"
+            label="Transaction Name"
+            size="small"
+            helperText="Required"
+            name="trxName"
+            value={trxName}
+            onChange={onChange}
+          />
+          <TextField
+            required={true}
+            variant="standard"
+            label="Transaction Type"
+            size="small"
+            name="type"
+            select
+            helperText="Required"
+            value={type}
+            onChange={onChange}
+          >
+            <MenuItem key="sale" value="sale">
+              Sale
+            </MenuItem>
+            <MenuItem key="listing" value="listing">
+              Listing
+            </MenuItem>
+            <MenuItem key="referral" value="referral">
+              Referral
+            </MenuItem>
+          </TextField>
+
+          <FormControl fullWidth className={classes.margin}>
+            <InputLabel htmlFor="standard-adornment-amount">
+              Associated Costs
+            </InputLabel>
+            <Input
+              id="standard-adornment-amount"
+              name="cost"
+              type="number"
+              value={cost}
+              onChange={onChange}
+              startAdornment={
+                <InputAdornment position="start">$</InputAdornment>
+              }
+            />
+          </FormControl>
+
+          <FormControl fullWidth className={classes.margin}>
+            <InputLabel htmlFor="standard-adornment-amount">
+              Expected Revenue
+            </InputLabel>
+            <Input
+              id="standard-adornment-amount"
+              name="revenue"
+              type="number"
+              value={revenue}
+              onChange={onChange}
+              startAdornment={
+                <InputAdornment position="start">$</InputAdornment>
+              }
+            />
+          </FormControl>
+
+          <KeyboardDatePicker
+            margin="normal"
+            id="date-picker-dialog"
+            label="Date"
+            format="MM/dd/yyyy"
+            value={dateOpened}
+            name="dateOpened"
+            //! Needs on change handler
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+
+        </Box>
         <Button
           variant="outlined"
-          fullWidth={true}
           type="submit"
-          color="secondary"
-          style={{ marginBottom: "1rem" }}
-          onClick={clearAll}
+          color="primary"
+          fullWidth={true}
+          style={{ marginTop: "1rem", marginBottom: "1rem" }}
+          onClick={handleClose}
         >
-          Clear
+          Submit
         </Button>
-      )}
+        {current && (
+          <Button
+            variant="outlined"
+            fullWidth={true}
+            type="submit"
+            color="secondary"
+            style={{ marginBottom: "1rem" }}
+            onClick={clearAll}
+          >
+            Clear
+          </Button>
+        )}
+      </MuiPickersUtilsProvider>
     </form>
   );
 };
