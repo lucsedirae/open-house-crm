2180b1df7cfe5603258be470e759d58fea1e93cc
David Stinnett <davidstinnett@icloud.com>
2021-02-02 18:28:35 -0500

    started working on charts, pushing so code is more easily readable by group

--- client/src/components/charts/Revenue.js
+++ client/src/components/charts/Revenue.js
@@ -1,5 +1,5 @@
 //* Dependencies
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 
 //* Material UI components, hooks, and icons
@@ -16,63 +16,11 @@ import { makeStyles } from "@material-ui/core/styles";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import FaceIcon from "@material-ui/icons/Face";
+import TransactionContext from "../../context/transactions/transactionContext";
 
 //* State context
 
 import { Bar } from "react-chartjs-2";
-
-//* Dummy Data
-const data = {
-	labels: [
-		"January",
-		"February",
-		"March",
-		"April",
-		"May",
-		"June",
-		"July",
-		"August",
-		"September",
-		"October",
-		"November",
-		"December",
-	],
-	datasets: [
-		{
-			label: "Revenue in thousands",
-			data: [12, 19, 3, 5, 3, 3, 15, 4, 6, 5, 11, 3],
-			backgroundColor: [
-				"rgb(21, 138, 12)",
-				"rgb(21, 138, 12)",
-				"rgb(21, 138, 12)",
-				"rgb(21, 138, 12)",
-				"rgb(21, 138, 12)",
-				"rgb(21, 138, 12)",
-				"rgb(21, 138, 12)",
-				"rgb(21, 138, 12)",
-				"rgb(21, 138, 12)",
-				"rgb(21, 138, 12)",
-				"rgb(21, 138, 12)",
-				"rgb(21, 138, 12)",
-			],
-			borderColor: [
-				"rgba(255, 99, 132, 1)",
-				"rgba(54, 162, 235, 1)",
-				"rgba(255, 206, 86, 1)",
-				"rgba(75, 192, 192, 1)",
-				"rgba(153, 102, 255, 1)",
-				"rgba(255, 159, 64, 1)",
-				"rgba(255, 99, 132, 1)",
-				"rgba(54, 162, 235, 1)",
-				"rgba(255, 206, 86, 1)",
-				"rgba(75, 192, 192, 1)",
-				"rgba(153, 102, 255, 1)",
-				"rgba(255, 159, 64, 1)",
-			],
-			borderWidth: 1,
-		},
-	],
-};
 
 //* Defines styles to be served via makeStyles MUI hook
 const useStyles = makeStyles({
@@ -96,6 +44,98 @@ const useStyles = makeStyles({
 
 //* Exported component
 const BarChart = () => {
+	const transactionContext = useContext(TransactionContext);
+	const {
+		transactions,
+		getTransactions,
+		loading,
+		setCurrentTrx,
+	} = transactionContext;
+
+	const [transaction, setTransaction] = useState({
+		trxName: "",
+		type: "",
+		cost: "",
+		revenue: "",
+		dateOpened: "",
+		dateClosed: "",
+		expectedCloseDate: "",
+	});
+
+	const {
+		trxName,
+		type,
+		cost,
+		revenue,
+		dateOpened,
+		dateClosed,
+		expectedCloseDate,
+	} = transaction;
+
+	useEffect(() => {
+		getTransactions();
+		// eslint-disable-next-line
+	}, []);
+
+	const revenueData =
+		transactions !== null && !loading
+			? transactions.map((transaction) => ({
+					cost: transaction.revenue,
+			  }))
+			: console.log("error");
+
+	const data = {
+		labels: [
+			"January",
+			"February",
+			"March",
+			"April",
+			"May",
+			"June",
+			"July",
+			"August",
+			"September",
+			"October",
+			"November",
+			"December",
+		],
+		datasets: [
+			{
+				label: "Revenue in thousands",
+				data: revenueData,
+				backgroundColor: [
+					"rgb(21, 138, 12)",
+					"rgb(21, 138, 12)",
+					"rgb(21, 138, 12)",
+					"rgb(21, 138, 12)",
+					"rgb(21, 138, 12)",
+					"rgb(21, 138, 12)",
+					"rgb(21, 138, 12)",
+					"rgb(21, 138, 12)",
+					"rgb(21, 138, 12)",
+					"rgb(21, 138, 12)",
+					"rgb(21, 138, 12)",
+					"rgb(21, 138, 12)",
+				],
+				borderColor: [
+					"rgba(255, 99, 132, 1)",
+					"rgba(54, 162, 235, 1)",
+					"rgba(255, 206, 86, 1)",
+					"rgba(75, 192, 192, 1)",
+					"rgba(153, 102, 255, 1)",
+					"rgba(255, 159, 64, 1)",
+					"rgba(255, 99, 132, 1)",
+					"rgba(54, 162, 235, 1)",
+					"rgba(255, 206, 86, 1)",
+					"rgba(75, 192, 192, 1)",
+					"rgba(153, 102, 255, 1)",
+					"rgba(255, 159, 64, 1)",
+				],
+				borderWidth: 1,
+			},
+		],
+	};
+
 	//* Initializes styling classes
 	const classes = useStyles();
 
--- client/src/context/transactions/TransactionState.js
+++ client/src/context/transactions/TransactionState.js
@@ -5,149 +5,149 @@ import transactionReducer from "./transactionReducer";
 
 //* Action types
 import {
-  ADD_TRANSACTION,
-  CLEAR_TRANSACTIONS,
-  CLEAR_CURRENT_TRX,
-  CLEAR_TRX_FILTER,
-  TRANSACTION_ERROR,
-  DELETE_TRANSACTION,
-  FILTER_TRANSACTIONS,
-  GET_TRANSACTIONS,
-  SET_CURRENT_TRX,
-  UPDATE_TRANSACTION
+	ADD_TRANSACTION,
+	CLEAR_TRANSACTIONS,
+	CLEAR_CURRENT_TRX,
+	CLEAR_TRX_FILTER,
+	TRANSACTION_ERROR,
+	DELETE_TRANSACTION,
+	FILTER_TRANSACTIONS,
+	GET_TRANSACTIONS,
+	SET_CURRENT_TRX,
+	UPDATE_TRANSACTION,
 } from "../types";
 
 //* State context
 import TransactionContext from "./transactionContext";
 
 const TransactionState = (props) => {
-  const initialState = {
-    transactions: null,
-    current: null,
-    filtered: null,
-    error: null
-  };
+	const initialState = {
+		transactions: null,
+		current: null,
+		filtered: null,
+		error: null,
+	};
 
-  const [state, dispatch] = useReducer(transactionReducer, initialState);
+	const [state, dispatch] = useReducer(transactionReducer, initialState);
 
-  //* Get Transactions
-  const getTransactions = async () => {
-    try {
-      const res = await axios.get("/api/transactions");
+	//* Get Transactions
+	const getTransactions = async () => {
+		try {
+			const res = await axios.get("/api/transactions");
 
-      dispatch({ type: GET_TRANSACTIONS, payload: res.data });
-    } catch (err) {
-      dispatch({ type: TRANSACTION_ERROR, payload: err.response.msg });
-    }
-  };
+			dispatch({ type: GET_TRANSACTIONS, payload: res.data });
+			console.log(res.data);
+		} catch (err) {
+			dispatch({ type: TRANSACTION_ERROR, payload: err.response.msg });
+		}
+	};
 
-  //* Add Transaction
-  const addTransaction = async (transaction) => {
-    const config = {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    };
+	//* Add Transaction
+	const addTransaction = async (transaction) => {
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
 
-    try {
-      const res = await axios.post("/api/transactions", transaction, config);
+		try {
+			const res = await axios.post("/api/transactions", transaction, config);
 
-      dispatch({ type: ADD_TRANSACTION, payload: res.data });
-    } catch (err) {
-      dispatch({ type: TRANSACTION_ERROR, payload: err.response.msg });
-    }
-  };
+			dispatch({ type: ADD_TRANSACTION, payload: res.data });
+		} catch (err) {
+			dispatch({ type: TRANSACTION_ERROR, payload: err.response.msg });
+		}
+	};
 
-  //* Delete Transaction
-  const deleteTransaction = async (id) => {
-    try {
-      await axios.delete(`/api/transactions/${id}`);
+	//* Delete Transaction
+	const deleteTransaction = async (id) => {
+		try {
+			await axios.delete(`/api/transactions/${id}`);
 
-      dispatch({
-        type: DELETE_TRANSACTION,
-        payload: id
-      });
-    } catch (err) {
-      dispatch({
-        type: TRANSACTION_ERROR,
-        payload: err.response.msg
-      });
-    }
-  };
+			dispatch({
+				type: DELETE_TRANSACTION,
+				payload: id,
+			});
+		} catch (err) {
+			dispatch({
+				type: TRANSACTION_ERROR,
+				payload: err.response.msg,
+			});
+		}
+	};
 
-  //* Clear transactions
-  const clearTransactions = () => {
-    dispatch({ type: CLEAR_TRANSACTIONS });
-  };
+	//* Clear transactions
+	const clearTransactions = () => {
+		dispatch({ type: CLEAR_TRANSACTIONS });
+	};
 
-  //* Set current transaction
-  const setCurrentTrx = (transaction) => {
-    dispatch({ type: SET_CURRENT_TRX, payload: transaction });
-  };
+	//* Set current transaction
+	const setCurrentTrx = (transaction) => {
+		dispatch({ type: SET_CURRENT_TRX, payload: transaction });
+	};
 
-  //* Clear current transaction
-  const clearCurrentTrx = () => {
-    dispatch({ type: CLEAR_CURRENT_TRX });
-  };
+	//* Clear current transaction
+	const clearCurrentTrx = () => {
+		dispatch({ type: CLEAR_CURRENT_TRX });
+	};
 
-  //* Update Transaction
-  const updateTransaction = async (transaction) => {
-    const config = {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    };
+	//* Update Transaction
+	const updateTransaction = async (transaction) => {
+		const config = {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		};
 
-    try {
-      const res = await axios.put(
-        `/api/transactions/${transaction._id}`,
-        transaction,
-        config
-      );
+		try {
+			const res = await axios.put(
+				`/api/transactions/${transaction._id}`,
+				transaction,
+				config
+			);
 
-      dispatch({
-        type: UPDATE_TRANSACTION,
-        payload: res.data
-      });
-    } catch (err) {
-      dispatch({
-        type: TRANSACTION_ERROR,
-        payload: err.response.msg
-      });
-    }
-  };
+			dispatch({
+				type: UPDATE_TRANSACTION,
+				payload: res.data,
+			});
+		} catch (err) {
+			dispatch({
+				type: TRANSACTION_ERROR,
+				payload: err.response.msg,
+			});
+		}
+	};
 
-  //* Filter Transactions
-  const filterTransactions = (text) => {
-    dispatch({ type: FILTER_TRANSACTIONS, payload: text });
-  };
+	//* Filter Transactions
+	const filterTransactions = (text) => {
+		dispatch({ type: FILTER_TRANSACTIONS, payload: text });
+	};
 
-  //* Clear Filter
-  const clearFilter = () => {
-    dispatch({ type: CLEAR_TRX_FILTER });
-  };
+	//* Clear Filter
+	const clearFilter = () => {
+		dispatch({ type: CLEAR_TRX_FILTER });
+	};
 
-  return (
-    <TransactionContext.Provider
-      value={{
-        transactions: state.transactions,
-        current: state.current,
-        error: state.error,
-        filtered: state.filtered,
-        addTransaction,
-        clearTransactions,
-        clearCurrentTrx,
-        clearFilter,
-        deleteTransaction,
-        filterTransactions,
-        getTransactions,
-        setCurrentTrx,
-        updateTransaction
-      }}
-    >
-      {props.children}
-    </TransactionContext.Provider>
-  );
+	return (
+		<TransactionContext.Provider
+			value={{
+				transactions: state.transactions,
+				current: state.current,
+				error: state.error,
+				filtered: state.filtered,
+				addTransaction,
+				clearTransactions,
+				clearCurrentTrx,
+				clearFilter,
+				deleteTransaction,
+				filterTransactions,
+				getTransactions,
+				setCurrentTrx,
+				updateTransaction,
+			}}>
+			{props.children}
+		</TransactionContext.Provider>
+	);
 };
 
 export default TransactionState;
