f4d89d4753145cc43dbb45c708968306ee9f8ab0
Tanner Kirkpatrick <twk4491@gmail.com>
2021-02-06 19:41:59 -0500

    react-moment installed as a dependency for handling date formats

--- client/src/components/forum/PostItem.js
+++ client/src/components/forum/PostItem.js
@@ -4,9 +4,11 @@ import Badge from "@material-ui/core/Badge";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
 import ReplyForm from "./ReplyForm";
+import AccountBoxIcon from "@material-ui/icons/AccountBox";
+import Moment from "react-moment";
 
 const PostItem = ({ post }) => {
-  const { name, title, body, replies, _id } = post;
+  const { name, title, body, replies, _id, date } = post;
 
   return (
     <div>
@@ -14,7 +16,10 @@ const PostItem = ({ post }) => {
         <Grid container spacing={3} alignItems="center" justify="center">
           <Grid item xs={12} sm={6} md={8}>
             <h1>{title}</h1>
-            <h4>By {name}</h4>
+            <Moment format="MM/DD/YYYY">{date}</Moment>
+            <h4>
+              <AccountBoxIcon fontSize="large" /> {name}
+            </h4>
             <p>{body}</p>
             <Replies replies={replies} />
             <ReplyForm _id={_id} />
